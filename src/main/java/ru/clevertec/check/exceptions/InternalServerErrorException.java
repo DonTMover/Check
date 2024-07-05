aa2f003214ac0ee513429f7fd89196c3459c6a69
Egor <46960051+DonTMover@users.noreply.github.com>
2024-07-05 13:12:19 +0300

    Update InternalServerErrorException.java

--- src/main/java/ru/clevertec/check/exceptions/InternalServerErrorException.java
+++ src/main/java/ru/clevertec/check/exceptions/InternalServerErrorException.java
@@ -1,5 +1,26 @@
 package ru.clevertec.check.exceptions;
 
-public class InternalServerErrorException extends RuntimeException{
-    public InternalServerErrorException(String message){super(message);}
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class InternalServerErrorException extends RuntimeException {
+    public InternalServerErrorException(String message) {
+        super(message);
+        try(FileWriter fw = new FileWriter("result.csv")) {
+            fw.write("ERROR\n");
+            fw.write("INTERNAL SERVER ERROR\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public InternalServerErrorException(String message, String filename) {
+        super(message);
+        try(FileWriter fw = new FileWriter(filename)) {
+            fw.write("ERROR\n");
+            fw.write("INTERNAL SERVER ERROR\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
