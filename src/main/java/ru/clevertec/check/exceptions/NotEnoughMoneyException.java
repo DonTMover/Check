1d03390746bb8c24593dd83cc717b05f0e1add67
Egor <46960051+DonTMover@users.noreply.github.com>
2024-07-05 13:12:28 +0300

    Update NotEnoughMoneyException.java

--- src/main/java/ru/clevertec/check/exceptions/NotEnoughMoneyException.java
+++ src/main/java/ru/clevertec/check/exceptions/NotEnoughMoneyException.java
@@ -1,5 +1,26 @@
 package ru.clevertec.check.exceptions;
 
-public class NotEnoughMoneyException extends RuntimeException{
-    public NotEnoughMoneyException(String message){super(message);}
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class NotEnoughMoneyException extends RuntimeException {
+    public NotEnoughMoneyException(String message) {
+        super(message);
+        try(FileWriter fw = new FileWriter("result.csv")) {
+            fw.write("ERROR\n");
+            fw.write("NOT ENOUGH MONEY\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public NotEnoughMoneyException(String message,String filename) {
+        super(message);
+        try(FileWriter fw = new FileWriter(filename)) {
+            fw.write("ERROR\n");
+            fw.write("NOT ENOUGH MONEY\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
