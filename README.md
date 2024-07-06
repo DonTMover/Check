636fc08b9e162a6988933fb190f74d78217380f3
DonTMover <antantov624@gmail.com>
2024-07-07 00:48:24 +0300

    Изменен README.md и дополнен SqlQueries

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Check
-Стек:Java 21, Gradle 8.5+
+Стек:Java 21, Gradle 8.5+, PostgreSQL 16+
 Инструкция по запуску: 
 ```
-java -cp src ./src/main/java/ru/clevertec/check/CheckRunner.java id-quantity id-quantity discountCard=XXXX balanceDebitCard=Number pathToFile=xxxx saveToFile=xxxx
+java -cp src ./src/main/java/ru/clevertec/check/CheckRunner.java id-quantity id-quantity discountCard=XXXX balanceDebitCard=Number datasource.url=ххх datasource.username=ххх datasource.password=ххх saveToFile=xxxx
 ```
 ## balanceDebitCard
 Баланс может быть ><= 0 (больше,меньше,равен)  
@@ -10,16 +10,45 @@ java -cp src ./src/main/java/ru/clevertec/check/CheckRunner.java id-quantity id-
 ## discountCard
 Скидочную карту можно не указывать, любая скидочная карта кроме* некоторых имеет 2% скидки  
 * Некоторые имеется ввиду от списка которые можно посмотреть [здесь](https://github.com/DonTMover/Check/blob/entry-core/src/main/resources/discountCards.csv)
-## pathToFile
-В этом отвлетвлении от основного кода добавлена поддержка сторонних csv файлов  
-Если данный агрумент не будет указан -> ```throw new BadRequestException```
-* Вам требуется его указать, в нем должна находиться бд содержащщая продукты типо [такого](https://github.com/DonTMover/Check/blob/entry-file/src/main/resources/products.csv)  
-* **Нужно указывать относительный путь**
+
+[//]: # (## pathToFile)
+
+[//]: # (В этом отвлетвлении от основного кода добавлена поддержка сторонних csv файлов  )
+
+[//]: # (Если данный агрумент не будет указан -> ```throw new BadRequestException```)
+
+[//]: # (* Вам требуется его указать, в нем должна находиться бд содержащщая продукты типо [такого]&#40;https://github.com/DonTMover/Check/blob/entry-file/src/main/resources/products.csv&#41;  )
+
+[//]: # (* **Нужно указывать относительный путь**)
+
+## datasource.url
+
+* Требуется указать, иначе Exception.
+* Вид: `jdbc:postgresql://example.com:port/Example`
+### Разбор вида:
+`example.com` - ссылка на сервер, на котором располагается База Данных  
+`port` - порт по которому работает DataBase, стандартный 5432  
+`Example` - это название Базы данных
+
+
+## datasource.username
+
+* Требуется указать, иначе Exception
+* Вид: `datasource.username=postgresql`, где `postgresql` нужно указать имя пользователя
+
+## datasource.password
+
+* Требуется указать, иначе Exception
+* Вид `datasource.password=postgresql`, где `postgresql` нужно указать пароль
+
 ## saveToFile
 Вместе с `pathToFile` так-же добавлен `saveToFile`, если его не указать, будет тоже самое исключение
 * **Нужно указывать относительный путь**
 
-## Другие случаи
-* Если аргумент `pathToFile` и `saveToFile` не были переданы, ошибка будет в `result.csv`
-* Если аргумент `pathToFile` не был передан, но был передан `saveToFile`, то ошибка будет в `saveToFile`
-* Если не передан `saveToFile`, то ошибка будет в `result.csv`
+[//]: # (## Другие случаи)
+
+[//]: # (* Если аргумент `pathToFile` и `saveToFile` не были переданы, ошибка будет в `result.csv`)
+
+[//]: # (* Если аргумент `pathToFile` не был передан, но был передан `saveToFile`, то ошибка будет в `saveToFile`)
+
+[//]: # (* Если не передан `saveToFile`, то ошибка будет в `result.csv`)
--- src/main/java/ru/clevertec/check/SqlQueries.java
+++ src/main/java/ru/clevertec/check/SqlQueries.java
@@ -1,15 +1,20 @@
 package ru.clevertec.check;
 
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.postgresql.Driver;
+import ru.clevertec.check.exceptions.BadRequestException;
+import ru.clevertec.check.exceptions.InternalServerErrorException;
 
 public class SqlQueries {
     private static final String DB_DRIVER = "org.postgresql.Driver";
     private static Connection connection;
 
-    protected static Connection getConnection(String datasourceUrl, String datasourceUsername, String datasourcePassword) {
+    protected static Connection getConnection(String datasourceUrl, String datasourceUsername, String datasourcePassword) throws IOException {
         if (connection != null) {
             return connection;
         }
@@ -18,14 +23,57 @@ public class SqlQueries {
             Properties props = new Properties();
             props.setProperty("user", datasourceUsername);
             props.setProperty("password", datasourcePassword);
-            return DriverManager.getConnection(datasourceUrl, props);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            connection = DriverManager.getConnection(datasourceUrl, props);
+            return connection;
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new BadRequestException(e.toString());
         }
 
     }
+    protected static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new InternalServerErrorException(e.toString());
+            }
+        }
+    }
+    protected static List<Product> getProducts(Connection connection) throws SQLException {
+        if (connection == null) {
+            throw new InternalServerErrorException("connection is null");
+        }
+        List<Product> products = new ArrayList<>();
+        PreparedStatement ps = connection.prepareStatement("select * from product");
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            products.add(new Product.Builder()
+                    .setId(rs.getInt("id"))
+                    .setName(rs.getString("description"))
+                    .setPrice(rs.getDouble("price"))
+                    .setDiscount(rs.getBoolean("wholesale_product"))
+                    .setQuantityInStock(rs.getInt("quantity_in_stock"))
+                    .build());
+        }
+        return products;
+    }
+    protected static List<DiscountCard> getDiscountCards(Connection connection) throws SQLException {
+        if (connection == null) {
+            throw new InternalServerErrorException("connection is null");
+        }
+        List<DiscountCard> discountCards = new ArrayList<>();
+        PreparedStatement ps = connection.prepareStatement("select * from discount_card");
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            discountCards.add(new DiscountCard.Builder()
+                    .setDiscount(rs.getInt("amount"))
+                    .setCardNumber(String.valueOf(rs.getInt("number")))
+                    .setId(rs.getInt("id"))
+                    .build());
+        }
+        return discountCards;
+    }
+
 
 
 }
