6bbf6cbc23ba4006fca3ee1f58d36fe0d3296132
DonTMover <antantov624@gmail.com>
2024-07-07 13:28:17 +0300

    added data.sql

--- src/main/java/ru/clevertec/check/SqlQueries.java
+++ src/main/java/ru/clevertec/check/SqlQueries.java
@@ -74,7 +74,4 @@ public class SqlQueries {
         }
         return discountCards;
     }
-
-
-
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+-- DDL
+create table product
+(
+    id                bigserial
+        primary key,
+    description       varchar(50) not null,
+    price             numeric     not null,
+    quantity_in_stock integer     not null,
+    wholesale_product boolean     not null
+);
+alter table product
+    owner to postgres;
+
+create table discount_card
+(
+    id     bigserial
+        primary key,
+    number integer  not null,
+    amount smallint not null
+);
+
+alter table discount_card
+    owner to postgres;
+
+-- DML
+select * from discount_card;
+select * from product;
